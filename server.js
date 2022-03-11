4dd93310adf3397c2a93db538914130b6b1f2e51
Ammar Bashir <ammarbashirmahoon@gmail.com>
2022-03-11 15:48:04 +0500

    11 march task

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const app = require('./src/app');
+const express = require ("express");
+const { contentType } = require("express/lib/response");
+const app = express();
 
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is live on port ${PORT}.`);
+app.post( "/login",(req, res)=>{
+    var nameformat = (/^[A-Za-z\s]*$/)
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var passformat=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    var phoneno = /^\d{11}$/
+    if(req.body.name.match(nameformat) && 
+    req.body.email.match(mailformat) && 
+    req.body.password.match(passformat)&&
+    req.body.phoneNumber.match(phoneno)){
+        res.send({message: "Registered successfully"});
+    }
+else
+{
+    res.status(401).send({ error : "Invalid Credentials" });
+}
+})
+
+app.listen(4000 , ()=>{
+    console.log("server is live on port 4000")
 });
